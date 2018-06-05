1f8ff17b6d8ee4d7b0964b4c54c7b736903e63eb
Balogun Abiodun Delight <delighteddell#@gmail.com>
2018-06-05 03:39:23 +0100

    second test case

--- test.js
+++ test.js
@@ -8,4 +8,11 @@ describe('SimpleOperations', () => {
     expect(operationResult).to.equal(55);
     done();
   });
+
+  it('should return numbers needed if the array contains a non numberic element',
+    (done) => {
+      const operationResult = arraySum([1,2,'delight',3,4,5,6,7,8,9,10]);
+      expect(operationResult).to.equal('Numbers needed');
+      done();
+  });
 });
